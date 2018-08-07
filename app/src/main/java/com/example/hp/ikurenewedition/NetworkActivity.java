ea9d5f77ec2808d9e9deb93109fde2383197afc5
agnibrainhack <chakrabortynilagnik@gmail.com>
2018-08-07 22:07:52 +0530

    Nilagnik v8 not working

--- app/src/main/java/com/example/hp/ikurenewedition/NetworkActivity.java
+++ app/src/main/java/com/example/hp/ikurenewedition/NetworkActivity.java
@@ -1,14 +1,18 @@
 package com.example.hp.ikurenewedition;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -52,6 +56,7 @@ public class NetworkActivity extends AppCompatActivity implements SwipeRefreshLa
     private CardView mCardView;
     private boolean start;
     private String from;
+    private String m_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,7 +140,10 @@ public class NetworkActivity extends AppCompatActivity implements SwipeRefreshLa
                     if (result != null) {
                         if (result.body().getPatientlist().size() > 0) {
                             for (int i = 0; i < result.body().getPatientlist().size(); i++) {
-                                dy.add(i, new Data_class(result.body().getPatientlist().get(i).getPid(),
+                                String data = result.body().getPatientlist().get(i).getPid();
+                                if(from.equals("parent") || from.equals("parent"))
+                                    data = "---";
+                                dy.add(i, new Data_class(data,
                                         result.body().getPatientlist().get(i).getName(),
                                         result.body().getPatientlist().get(i).getAge()));
                             }
@@ -160,20 +168,63 @@ public class NetworkActivity extends AppCompatActivity implements SwipeRefreshLa
                                 Intent k = null;
                                 if(from.equals("parent")) {
                                     k = new Intent(NetworkActivity.this, OnePerson.class);
+                                    k.putExtra("patient", url);
+                                    k.putExtra("card_no", patient);
+                                    k.putExtra("patient_name", name);
+                                    startActivity(k);
+                                    overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
                                 }
                                 if(from.equals("child")) {
-                                    k = new Intent(NetworkActivity.this, ChildViewActivity.class);
-                                }
+
+
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(NetworkActivity.this);
+                                    builder.setTitle("Enter PID");
+
+// Set up the input
+                                    final EditText input = new EditText(NetworkActivity.this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                    builder.setView(input);
+
+// Set up the buttons
+                                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            m_Text = input.getText().toString();
+                                        }
+                                    });
+                                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+
+                                    builder.show();
+
+                                    if(m_Text.equals(result.body().getPatientlist().get(position).getPid())){
+                                        k = new Intent(NetworkActivity.this, ChildViewActivity.class);
+                                        k.putExtra("patient", url);
+                                        k.putExtra("card_no", patient);
+                                        k.putExtra("patient_name", name);
+                                        startActivity(k);
+                                        overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+                                    }
+                                    else {
+                                        Toast.makeText(NetworkActivity.this, "Wrong Credentials", Toast.LENGTH_LONG).show();
+                                    }
+                                    }
                                 if(from.equals("vol")) {
                                     k = new Intent(NetworkActivity.this, VolunteerActivity.class);
+                                    k.putExtra("patient", url);
+                                    k.putExtra("card_no", patient);
+                                    k.putExtra("patient_name", name);
+                                    startActivity(k);
+                                    overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
                                 }
 
                                 //String str = Integer.toString(position);
-                                k.putExtra("patient", url);
-                                k.putExtra("card_no", patient);
-                                k.putExtra("patient_name", name);
-                                startActivity(k);
-                                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+
                                 // pass the intent here
                             }
                         });
