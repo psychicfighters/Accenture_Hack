0727b72d7cca6c9d1cede435c4f5b722c5c03650
agnibrainhack <chakrabortynilagnik@gmail.com>
2018-08-10 13:27:50 +0530

    nilagnik v-15 working

--- app/src/main/java/com/example/hp/wecarenewedition/VolunteerActivity.java
+++ app/src/main/java/com/example/hp/wecarenewedition/VolunteerActivity.java
@@ -1,20 +1,34 @@
 package com.example.hp.wecarenewedition;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.hp.ikurenewedition.AfterSplash;
 import com.example.hp.ikurenewedition.NetworkActivity;
 import com.example.hp.ikurenewedition.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 
-public class VolunteerActivity extends AppCompatActivity {
+import java.io.File;
 
+public class VolunteerActivity extends AppCompatActivity {
+    boolean status = false;
     private String pid;
+    private File savefiledir;
+    private Uri photoUri;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +42,21 @@ public class VolunteerActivity extends AppCompatActivity {
         buttonecgCaptur.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(VolunteerActivity.this, VolunteerActivity.class);
-                startActivity(intent);
-
-            }
-        });
-        //capture prescription
-        Button buttonprescriptionCapture = findViewById(R.id.capture2);
-        buttonprescriptionCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(VolunteerActivity.this, VolunteerActivity.class);
-                startActivity(intent);
+                dispatchTakePictureIntent();
 
             }
         });
 
-        Button buttonprescription = findViewById(R.id.submit5);
-        buttonprescription.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(VolunteerActivity.this, VolunteerActivity.class);
-                startActivity(intent);
-
-            }
-        });
 
         Button buttonecg = findViewById(R.id.submit1);
         buttonecg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(VolunteerActivity.this, VolunteerActivity.class);
-                startActivity(intent);
+                if (status) {
 
+                } else {
+                    Toast.makeText(VolunteerActivity.this, "Please Select A Photo First", Toast.LENGTH_LONG).show();
+                }
             }
         });
         Button buttonbp = findViewById(R.id.submit2);
@@ -95,4 +91,65 @@ public class VolunteerActivity extends AppCompatActivity {
         });
 
     }
-}
+
+
+    private void dispatchTakePictureIntent() {
+
+        //imageView.setImageResource(0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA},
+                        MY_CAMERA_REQUEST_CODE);
+            }
+        }
+
+
+    }
+
+
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    Long yis = System.currentTimeMillis() / 1000;
+                    savefiledir = new File(getExternalFilesDir(null) + File.separator + "PresandECGReports" + pid + yis.toString() + ".jpeg");
+                    // String picname = getPicname();
+                    //File imageFile = new File(savefiledir, picname);
+                    photoUri = FileProvider.getUriForFile(getBaseContext(), getBaseContext().getApplicationContext().getPackageName() + ".com.example.hp.wecarenewedition.provider", savefiledir);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+                } else {
+
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+
+        }
+    }
+            @Override
+            protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                    status = true;
+                    //
+
+
+                   // status = false;  // to be done
+                }
+            }
+
+
+        }
+
