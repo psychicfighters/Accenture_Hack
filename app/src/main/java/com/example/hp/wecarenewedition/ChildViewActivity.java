6520ae552567a1e2e75f53b06dfe7a00b8e6c9e3
sysadminamit <amitc8250@gmail.com>
2018-08-12 02:02:01 +0530

    amit21

--- app/src/main/java/com/example/hp/wecarenewedition/ChildViewActivity.java
+++ app/src/main/java/com/example/hp/wecarenewedition/ChildViewActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import com.example.hp.ikurenewedition.R;
 import com.example.hp.ikurenewedition.dataclass.Data_class_four;
 import com.example.hp.ikurenewedition.pojodatamodels.BPDetails;
@@ -30,11 +29,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ChildViewActivity extends AppCompatActivity {
-    HorizontalBarChart mFastingBarChat;
-    HorizontalBarChart mPpBarChat;
-    HorizontalBarChart mRandomBarChat;
-    HorizontalBarChart mSystolicBarChat;
-    HorizontalBarChart mDiastolicBarChart;
+    HorizontalBarChart mSugarBarChat;
+    HorizontalBarChart mPressureChat;
+
     private ProgressDialog progressDialog;
     private ArrayList<Data_class_four> dy;
     int k1, k2, k3 = 0;
@@ -45,12 +42,12 @@ public class ChildViewActivity extends AppCompatActivity {
     private ArrayList<String> diab_random = new ArrayList<>();
     private ArrayList<String> diab_random_date = new ArrayList<>();
     private String pid;
+
     private ArrayList<String> syslist = new ArrayList<>();
     private ArrayList<String> dialist = new ArrayList<>();
     private TextView textview;
     private ArrayList<Float> timelist = new ArrayList<Float>();
     TextView textView2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,16 +56,8 @@ public class ChildViewActivity extends AppCompatActivity {
         pid = intent.getStringExtra("patient");
         textview = findViewById(R.id.last_checkup);
         textView2 = findViewById(R.id.timetilllast_checkup);
-
-        mFastingBarChat = (HorizontalBarChart)findViewById(R.id.sugar_fasting);
-
-        mPpBarChat = (HorizontalBarChart)findViewById(R.id.sugar_pp);
-
-        mRandomBarChat = (HorizontalBarChart)findViewById(R.id.sugar_random);
-
-        mSystolicBarChat = (HorizontalBarChart)findViewById(R.id.pressure_systolic);
-
-        mDiastolicBarChart = (HorizontalBarChart)findViewById(R.id.pressure_diastolic);
+        mSugarBarChat = (HorizontalBarChart)findViewById(R.id.sugar_barchart);
+        mPressureChat = (HorizontalBarChart)findViewById(R.id.pressure_barchart);
 
         callAPI1();
         callAPI2();
@@ -76,134 +65,49 @@ public class ChildViewActivity extends AppCompatActivity {
         //progressDialog.dismiss();
 
     }
-    /*private void setFastingData() {
-        float barWidth = 9f;
+
+    private void setSugarBarChart(int count,int range,float value){
         float spaceforBar = 10f;
-        ArrayList<String> sugarFasting = new ArrayList<String>();
-        sugarFasting.add("100");
-        BarDataSet barDataSet = new BarDataSet(sugarFasting,"mm/dl");
-        BarData data = new BarData(sugarFasting,);
 
-
-    }*/
-
-    private void setFastingData(int count,int range, int value) {
-        float barWidth = 9f;
-        float spaceforBar = 10f;
-        ArrayList<BarEntry> sugarFasting = new ArrayList<>();
-        //for (int i = 0; i <count; i++) {
-        //    float val = (float) (Math.random() * 50);
-         //   sugarFasting.add(new BarEntry(i*spaceforBar, (int) val));
-        //}
-        sugarFasting.add(new BarEntry(value, 0));
+        ArrayList<BarEntry> sugarBar = new ArrayList<>();
+        for (int i = 0;i<=count;i++){
+            float val = (float)(Math.random()*value);
+            sugarBar.add(new BarEntry(i*spaceforBar, (int) val));
+        }
         ArrayList<String> labels = new ArrayList<>();
-        labels.add(String.valueOf(value));
-        BarDataSet set2;
-        set2 = new BarDataSet(sugarFasting,"mm/dl");
+        labels.add("Fasting");
+        labels.add("PP");
+        labels.add("Random");
+        BarDataSet set1;
+        set1 = new BarDataSet(sugarBar,"mm/dl");
         if(value > 110)
-            set2.setColor(Color.parseColor("#c62828"));
+            set1.setColor(Color.parseColor("#c62828"));
         else
-            set2.setColor(Color.parseColor("#2e7d32"));
-
+            set1.setColor(Color.parseColor("#2e7d32"));
+        BarData data = new BarData(labels,set1);
+        //data.setBarWidth(barWidth);
+        mSugarBarChat.setData(data);
+        mSugarBarChat.animateY(600);
+        mSugarBarChat.invalidate();
+    }
+    private void setPressureBarChart(int count,int range,float value){
+        float spaceforBar = 10f;
+        ArrayList<BarEntry> pressureBar = new ArrayList<>();
+        for (int i =0; i<=count;i++){
+            float val = (float)(Math.random()*value);
+            pressureBar.add(new BarEntry(i*spaceforBar, (int) val));
+        }
+        ArrayList<String>labels = new ArrayList<>();
+        labels.add("Systolic");
+        labels.add("Diastolic");
+        BarDataSet set2 = new BarDataSet(pressureBar,"mm/dl");
         BarData data = new BarData(labels,set2);
         //data.setBarWidth(barWidth);
-        mFastingBarChat.setData(data);
-        mFastingBarChat.animateY(600);
+        mPressureChat.setData(data);
+        mPressureChat.animateY(1700);
+        set2.setColor(Color.parseColor("#FFFFCC00"));
+        mPressureChat.invalidate();
 
-
-    }
-
-    private void setPpData(int count,int range, int value) {
-        float barWidth = 9f;
-        float spaceforBar = 10f;
-        ArrayList<BarEntry> sugarpp = new ArrayList<>();
-//        for (int i = 0; i <count; i++) {
-//            float val1 = (float) (Math.random() * 70);
-//            sugarpp.add(new BarEntry(i*spaceforBar, (int) val1));
-//        }
-        sugarpp.add(new BarEntry(value, 0));
-        ArrayList<String> labels = new ArrayList<>();
-        labels.add(String.valueOf(value));
-        BarDataSet set21;
-        set21 = new BarDataSet(sugarpp,"mm/dl");
-        //set21.setBarSpacePercent(100f);
-        if(value > 150)
-            set21.setColor(Color.parseColor("#c62828"));
-        else
-            set21.setColor(Color.parseColor("#2e7d32"));
-        BarData data1 = new BarData(labels,set21);
-        //data.setBarWidth(barWidth);
-        mPpBarChat.setData(data1);
-
-        mPpBarChat.animateY(1000);
-
-
-    }
-    private void setRandomData(int count,int range, int value) {
-        float barWidth = 9f;
-        float spaceforBar = 10f;
-        ArrayList<BarEntry> sugarRandom = new ArrayList<>();
-        /*for (int i = 0; i <count; i++) {
-            float val = (float) (Math.random() * 80);
-            sugarRandom.add(new BarEntry(i*spaceforBar, (int) val));
-        }*/
-        sugarRandom.add(new BarEntry(value, 0));
-        ArrayList<String> labels1 = new ArrayList<>();
-        labels1.add(String.valueOf(value));
-        BarDataSet set2;
-        set2 = new BarDataSet(sugarRandom,"mm/dl");
-        if(value > 135)
-            set2.setColor(Color.parseColor("#c62828"));
-        else
-            set2.setColor(Color.parseColor("#2e7d32"));
-
-        BarData data = new BarData(labels1,set2);
-        //data.setBarWidth(barWidth);
-        mRandomBarChat.setData(data);
-        mRandomBarChat.animateY(1200);
-        //set2.setColor(Color.parseColor("#FFFF05F7"));
-
-
-
-    }
-    private void setSystolicData(int count,int range, int value) {
-        float barWidth = 9f;
-        float spaceforBar = 10f;
-        ArrayList<BarEntry> sugarSystolic = new ArrayList<>();
-       /* for (int i = 0; i <count; i++) {
-            float val = (float) (Math.random() * 40);
-            sugarSystolic.add(new BarEntry(i*spaceforBar, (int) val));
-        }*/
-        sugarSystolic.add(new BarEntry(value, 0));
-        ArrayList<String> labels = new ArrayList<>();
-        labels.add(String.valueOf(value));
-        BarDataSet set3;
-        set3 = new BarDataSet(sugarSystolic,"mm/dl");
-        BarData data = new BarData(labels,set3);
-        //data.setBarWidth(barWidth);
-        mSystolicBarChat.setData(data);
-        mSystolicBarChat.animateY(1700);
-        set3.setColor(Color.parseColor("#FFFFCC00"));
-    }
-    private void setDiastlicData(int count,int range, int value) {
-        float barWidth = 9f;
-        float spaceforBar = 10f;
-        ArrayList<BarEntry> sugarDiastolic = new ArrayList<>();
-        /*for (int i = 0; i <count; i++) {
-            float val3 = (float) (Math.random() * 20);
-            sugarDiastolic.add(new BarEntry(i*spaceforBar, (int) val3));
-        }*/
-
-        sugarDiastolic.add(new BarEntry(value, 0));
-        ArrayList<String> labels = new ArrayList<>();
-        labels.add(String.valueOf(value));
-        BarDataSet set2;
-        set2 = new BarDataSet(sugarDiastolic,"mm/dl");
-        BarData data = new BarData(labels,set2);
-        //data.setBarWidth(barWidth);
-        mDiastolicBarChart.setData(data);
-        mDiastolicBarChart.animateY(2000);
-        set2.setColor(Color.parseColor("#FF34AE00"));
     }
 
 
@@ -257,24 +161,24 @@ public class ChildViewActivity extends AppCompatActivity {
                     }
                 }
                 if(diab_fasting.get(0) == null){
-                    setFastingData(1, 250, 0);
+                    setSugarBarChart(0, 250, 0);
                 }
                 else{
-                    setFastingData(1, 250, Integer.parseInt(diab_fasting.get(0)));
+                    setSugarBarChart(0, 250, Float.parseFloat(diab_fasting.get(0)));
                 }
 
                 if(diab_pp.get(0) == null){
-                    setPpData(1, 350, 0);
+                    setSugarBarChart(1, 350, 0);
                 }
                 else{
-                    setPpData(1, 350, Integer.parseInt(diab_pp.get(0)));
+                    setSugarBarChart(1, 350, Float.parseFloat(diab_pp.get(0)));
                 }
 
                 if(diab_random.get(0) == null){
-                    setRandomData(1, 300, 0);
+                    setSugarBarChart(2, 300, 0);
                 }
                 else{
-                    setRandomData(1, 300, Integer.parseInt(diab_random.get(0)));
+                    setSugarBarChart(1, 300, Float.parseFloat(diab_random.get(0)));
                 }
             }
 
@@ -311,17 +215,17 @@ public class ChildViewActivity extends AppCompatActivity {
 
                 }
                 if(syslist.get(0) == null){
-                    setSystolicData(1, 200, 0);
+                    setPressureBarChart(0, 200, 0);
                 }
                 else{
-                    setSystolicData(1, 200, Integer.parseInt(syslist.get(0)));
+                    setPressureBarChart(0, 200, Float.parseFloat(syslist.get(0)));
                 }
 
                 if(dialist.get(0) == null){
-                    setDiastlicData(1, 200, 0);
+                    setPressureBarChart(1, 200, 0);
                 }
                 else{
-                    setDiastlicData(1, 200, Integer.parseInt(dialist.get(0)));
+                    setPressureBarChart(1, 200, Float.parseFloat(dialist.get(0)));
                 }
             }
 
